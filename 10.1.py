b0a074ca7a2e39be7e4d86a4d9e6f3846a32c889
Fouroxa <git config --global user.email>
2023-04-24 21:51:20 +0700

    Решение темы 10

---
+++ 10.1.py
@@ -0,0 +1,4 @@
+def year_to_day(year):
+    year *= 365
+    return year
+print(year_to_day(int(input("Введите свой возраст: "))))
---
+++ 10.py
@@ -0,0 +1,5 @@
+def min_to_sec(minute):
+    minute *= 60
+    return minute
+print(min_to_sec(int(input("Введите количество минут: "))))
+
