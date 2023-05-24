e7dde1334424d259a5c5294328fa089671aa8686
Fouroxa <git config --global user.email>
2023-05-24 14:48:33 +0700

    Правка

--- 10.2.2.py
+++ 10.2.2.py
@@ -1,6 +1,6 @@
 def three_args(var1, var2 = None, var3 = None):
-    list_arg = []
     dict_args = locals()
+    list_arg = []
     for key, values in dict_args.items():
         if values is not None:
             list_arg.append(f'{key} = {values}')
